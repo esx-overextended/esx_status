07d8e1f8dbc4e220eb0439b6d82d271a1fdbb6c9
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2024-11-13 18:48:47 -0500

    feat(server): keep track of players and init status system on their join
    not tested

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,69 @@
+local utils  = require("shared.utils")
 local config = require("shared.config")
 GlobalState:set("statuses", config.statuses, true)
+
+local tracker = require("class.PlayerStatusRegistry")()
+
+AddEventHandler("esx:playerLoaded", function(playerId, xPlayer)
+    if tracker:addPlayer(playerId, xPlayer.metadata.statuses) then
+        TriggerEvent("esx_status:playerAdded", playerId)
+        Entity(playerId).state:set("esx_status:loaded", true, true)
+    end
+end)
+
+AddEventHandler("esx:playerDropped", function(playerId)
+    if tracker:removePlayer(playerId) then
+        TriggerEvent("esx_status:playerRemoved", playerId)
+        Entity(playerId).state:set("esx_status:loaded", false, true)
+    end
+end)
+
+---Generates an export to retrieve the specified player's status value
+---@param playerId number
+---@param status string
+---@return number?
+function utils.api.getPlayerStatus(playerId, status)
+    local player = tracker:getPlayer(playerId)
+    local playerStatus = player and player:getStatus(status)
+
+    return playerStatus and playerStatus:getValue()
+end
+
+---Generates an export to set the specified player's status value
+---@param playerId number
+---@param status string
+---@param amount number
+---@return boolean?
+function utils.api.setPlayerStatus(playerId, status, amount)
+    local player = tracker:getPlayer(playerId)
+    local playerStatus = player and player:getStatus(status)
+
+    return playerStatus and playerStatus:setValue(amount)
+end
+
+---Generates an export to increase the specified player's status value
+---@param playerId number
+---@param status string
+---@param amount number
+---@return boolean?
+function utils.api.increasePlayerStatus(playerId, status, amount)
+    local player = tracker:getPlayer(playerId)
+    local playerStatus = player and player:getStatus(status)
+
+    return playerStatus and playerStatus:increaseValue(amount)
+end
+
+---Generates an export to decrease the specified player's status value
+---@param playerId number
+---@param status string
+---@param amount number
+---@return boolean?
+function utils.api.decreasePlayerStatus(playerId, status, amount)
+    local player = tracker:getPlayer(playerId)
+    local playerStatus = player and player:getStatus(status)
+
+    return playerStatus and playerStatus:decreaseValue(amount)
+end
 
 ---@param resource string
 local function onResourceStop(resource)
--- shared/utils.lua
+++ shared/utils.lua
@@ -6,4 +6,10 @@ function utils.isValueValid(amount)
     return type(amount) == "number" and (amount >= 0 and amount <= 100)
 end
 
+utils.api = setmetatable({}, {
+    __newindex = function(_, index, value)
+        return exports(index, value)
+    end
+})
+
 return utils
